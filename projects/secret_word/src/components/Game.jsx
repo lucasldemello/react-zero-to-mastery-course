6a608e2c95637038f88ebb9b37bcc6aa5ec281b6
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-05-28 23:52:43 -0300

    feat: Structure of game component

--- projects/secret_word/src/components/Game.jsx
+++ projects/secret_word/src/components/Game.jsx
@@ -2,9 +2,30 @@ import "./Game.css";
 
 const Game = ({ checkLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={checkLetter}>End game</button>
+    <div className="game">
+      <p className="points">
+        <span>Score: 000</span>
+      </p>
+      <h1>Guess the word</h1>
+      <h3 className="tip">
+        Hint about the word: <span>Hint</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Start guessing a letter:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" />
+          <button>Play</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer">
+        <p>Wrong letters:</p>
+        <span className="wrongLetter">B, </span>
+        <span className="wrongLetter">C</span>
+      </div>
     </div>
   );
 };
