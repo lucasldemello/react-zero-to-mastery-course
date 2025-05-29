d963b290654b3de31af19f162448493b4794af73
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-05-28 22:39:42 -0300

    feat(core): file with the words to play with

---
+++ projects/secret_word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    car: ["Tire", "Engine", "Steering Wheel", "Brake", "Accelerator"],
+    fruit: ["Apple", "Banana", "Cherry", "Peach", "Elderberry"],
+    body: ["Heart", "Lungs", "Liver", "Kidney", "Brain"],
+    computer: ["Monitor", "Keyboard", "Mouse", "CPU", "RAM"],
+    programming: ["Language", "Function", "Variable", "Loop", "Array"],
+    food: ["Pizza", "Burger", "Pasta", "Salad", "Sushi"],
+}
