d73239dead6dccf65d3442ed62f6bfaba3ef7849
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-02 21:02:49 -0300

    experiments: retrieving data from the API

--- experiments/httpreact/src/App.jsx
+++ experiments/httpreact/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [cards, setCards] = useState([]);
+  const url = "http://localhost:3000/cards";
+
+  // 1 - Fetch cards data from the API
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setCards(data);
+      } catch (error) {
+        console.error("Erro ao buscar os cards:", error);
+      }
+    }
+
+    fetchData();
+  }, []); // execute only once when the component mounts
 
   return (
     <>
       <div className="App">
-        <h1 className="cardsList">Bloomburrow cards list</h1>
+        <h1 className="cardsList">Cards list</h1>
+        <ul>
+          {cards.map((card) => (
+            <li key={card.id} className="card">
+              <h2>
+                {card.name} - US$: {card.price}
+              </h2>
+              <p>{card.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
