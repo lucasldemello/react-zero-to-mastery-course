614d80f24764a09b067d1dbba9e699a451624e11
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-02 21:46:06 -0300

    feat: custom hooks

--- experiments/httpreact/src/App.jsx
+++ experiments/httpreact/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
 function App() {
   const [cards, setCards] = useState([]);
   const url = "http://localhost:3000/cards";
@@ -10,19 +13,22 @@ function App() {
   const [description, setDescription] = useState("");
 
   // 1 - Fetch cards data from the API
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        setCards(data);
-      } catch (error) {
-        console.error("Erro ao buscar os cards:", error);
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const res = await fetch(url);
+  //       const data = await res.json();
+  //       setCards(data);
+  //     } catch (error) {
+  //       console.error("Erro ao buscar os cards:", error);
+  //     }
+  //   }
 
-    fetchData();
-  }, []); // execute only once when the component mounts
+  //   fetchData();
+  // }, []); // execute only once when the component mounts
+
+  // 4 - custom hook
+  const { data: items } = useFetch(url);
 
   // 2 - Add a card to the list
   const handleSubmit = async (event) => {
@@ -58,14 +64,15 @@ function App() {
       <div className="App">
         <h1 className="cardsList">Cards list</h1>
         <ul>
-          {cards.map((card) => (
-            <li key={card.id} className="card">
-              <h2>
-                {card.name} - US$: {card.price}
-              </h2>
-              <p>{card.description}</p>
-            </li>
-          ))}
+          {items &&
+            items.map((card) => (
+              <li key={card.id} className="card">
+                <h2>
+                  {card.name} - US$: {card.price}
+                </h2>
+                <p>{card.description}</p>
+              </li>
+            ))}
         </ul>
         <div className="add-card">
           <form onSubmit={handleSubmit}>
