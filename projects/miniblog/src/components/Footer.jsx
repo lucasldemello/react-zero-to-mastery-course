cb5c26fe2e14da1f062a1d65bef709476e6119d2
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-03 22:59:50 -0300

    style: css for navbar and footer

--- projects/miniblog/src/components/Footer.jsx
+++ projects/miniblog/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Write about what matters to you</h3>
       <p>Mini Blog &copy; 2025</p>
     </footer>
--- projects/miniblog/src/components/Navbar.jsx
+++ projects/miniblog/src/components/Navbar.jsx
@@ -5,15 +5,25 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <NavLink to="/" className={styles.logo}>
+      <NavLink to="/" className={styles.brand}>
         Mini <span>Blog</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            About
+          </NavLink>
         </li>
       </ul>
     </nav>
