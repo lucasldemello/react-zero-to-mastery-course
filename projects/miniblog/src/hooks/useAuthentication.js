573151a30ada72b67ee5a653818ac943a469f8d6
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-04 17:04:47 -0300

    feat: creating the auth hook

---
+++ projects/miniblog/src/hooks/useAuthentication.js
@@ -0,0 +1,26 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // cleanup
+  // TODO: study about memory leaks in React
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+};
