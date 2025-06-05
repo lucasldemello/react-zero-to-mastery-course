59ab5dff938754df4b704c44dee7c191acb8da51
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-05 17:55:07 -0300

    feat: Blocking pages which need auth

--- projects/miniblog/src/pages/About/About.jsx
+++ projects/miniblog/src/pages/About/About.jsx
@@ -1,10 +1,20 @@
 // CSS
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        About the Mini <span>Blog</span>
+      </h2>
+      <p>
+        This is a simple blog application built with React and Firebase. It
+        allows users to create, read, update, and delete posts.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Create Post
+      </Link>
     </div>
   );
 };
