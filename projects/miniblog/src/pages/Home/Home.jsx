33912345a7cbf3600f443f91cbb316580aedc946
Lucas Luan de Melo Pereira <lucasluandemelo@gmail.com>
2025-06-06 17:57:18 -0300

    feat: Implement Home component with search functionality and no posts message

--- projects/miniblog/src/pages/Home/Home.jsx
+++ projects/miniblog/src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
 // CSS
 import styles from "./Home.module.css";
 
+// Hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>See our latest posts</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Or search for tags..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div className="post-list">
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Found no posts</p>
+            <p>Would you like to create the first post?</p>
+            <Link to="/posts/create" className="btn">
+              Yes!
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
