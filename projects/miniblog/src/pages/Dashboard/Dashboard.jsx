5e7de46ba38312f6f0ad1185229f18d217dc4ccf
Lucas Luan de Melo <lucasluandemelo@gmail.com>
2025-06-14 16:48:01 -0300

    feat: Implement Dashboard layout with user post management and no posts message

--- projects/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ projects/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,34 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  // user posts
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p> Manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>You have not created any posts yet.</p>
+          <Link to="/posts/create" className="btn">
+            Create your first post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Your posts</p>
+        </div>
+      )}
     </div>
   );
 };
