7dca8aee6d988f84848f68860e937fad1932dea5
Lucas Luan de Melo <lucasluandemelo@gmail.com>
2025-06-14 15:51:18 -0300

    feat: Enhance Search page layout and display results with no posts message

--- projects/miniblog/src/pages/Search/Search.jsx
+++ projects/miniblog/src/pages/Search/Search.jsx
@@ -4,13 +4,30 @@ import styles from "./Search.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 
+// components
+import PostDetail from "../../components/PostDetail";
+import { Link } from "react-router-dom";
+
 const Search = () => {
   const query = useQuery();
-  const search = query.get("q") || "";
-  
+  const search = query.get("q");
+
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
-      <h2>Search: {search}</h2>
+    <div className={styles.search_container}>
+      <h1>Found {posts ? posts.length : 0} results for "{search}"</h1>
+      <div className="post-list">
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>We couldn't find any results for your search.</p>
+            <Link to="/" className="btn btn-dark">
+              Go back
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
