4d837b6fb28dfe8390a8a3a91d67fc6796de8ea7
Lucas Luan de Melo <lucasluandemelo@gmail.com>
2025-08-22 00:10:14 -0300

    feat: Add CSS styling for button and implement height input form in HookUseState component

--- experiments/hooks/reacthooks/src/components/HookUseState.jsx
+++ experiments/hooks/reacthooks/src/components/HookUseState.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+
+import "./HookUseState.css";
+
 const HookUseState = () => {
   // 1 - useState
   let userName = "Lucas";
@@ -8,6 +11,18 @@ const HookUseState = () => {
   const changeNames = () => {
     userName = "Lucas de Melo";
     setName("J. R. R. Tolkien");
+
+    console.log("userName:", userName);
+  };
+
+  console.log("name:", name);
+
+  // 2 - useState and input
+  const [height, setHeight] = useState(170);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Height submitted:", height);
   };
 
   return (
@@ -17,6 +32,17 @@ const HookUseState = () => {
       <p> variable: {userName}</p>
       <p> useState: {name}</p>
       <button onClick={changeNames}>Change Names</button>
+      {/* 2 - useState and input  */}
+      <form onSubmit={handleSubmit}>
+        <p>Type your height (cm)</p>
+        <input
+          type="text"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>Your height is {height} cm.</p>
       <hr />
     </div>
   );
