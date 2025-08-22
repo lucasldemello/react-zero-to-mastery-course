0f7cd5478da492295ae24c299d3b87c40f1eb2cb
Lucas Luan de Melo <lucasluandemelo@gmail.com>
2025-08-22 01:00:34 -0300

    experiment: Using useReducer with events.

--- experiments/hooks/reacthooks/src/components/HookUseReducer.jsx
+++ experiments/hooks/reacthooks/src/components/HookUseReducer.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   // 1 - Starting with useReducer
@@ -7,12 +7,70 @@ const HookUseReducer = () => {
     return Math.random(state);
   });
 
+  // 2 - Advancing in useReducer
+  const initialTasks = [
+    {
+      id: 1,
+      text: "Learn React",
+    },
+    {
+      id: 2,
+      text: "Learn GraphQL",
+    },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_TASK":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+
+        setTaskText("");
+        return [...state, newTask]; // study spread operator.
+      case "REMOVE_TASK":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatchTasks({ type: "ADD_TASK", payload: taskText });
+  };
+
   return (
     <div>
       <h2>useReducer</h2>
       <p>Number: {number}</p>
       <button onClick={dispatch}>Randomize the number</button>
       <hr />
+      <h3>Tasks</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+        />
+        <input type="submit" value="Add Task" />
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            onDoubleClick={() =>
+              dispatchTasks({ type: "REMOVE_TASK", id: task.id })
+            }
+          >
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
